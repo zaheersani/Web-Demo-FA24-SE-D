9e69adf83bcc8d2506d37841db11b4db5079edae
Zaheer Sani <sani.zaheer@gmail.com>
2024-10-15 12:59:40 +0500

    Middleware

--- index.js
+++ index.js
@@ -1,59 +1,79 @@
-const express = require("express")
+const express = require('express')
 const app = express()
-// importing users
-// const users = require('./users')
-// const users = require('./users.json')
+const users = require('./users.json')
 
+// Builtin Middleware
 app.use(express.json())
 
-const userRouter = require('./usersRouter')
-app.use('/users', userRouter)
+// Custom Middleware
+const middleware1 = (req, res, next) => {
+    console.log('Middleware 1')
+    console.log(`${req.method} ${req.path}`)
+    req.info = "Some info"
+    // req.method = "POST"
+    next()
+    // res.send('I am not happy with your request!')
+}
 
-app.get('/', (req, res) => {
-    // res.send('Hello World from Root')
-    // JavaScript Object Notation
-    const json = '{"name":"John", "age":35}';
-    
-    const user = {
-        name: 'John',
-        age: 30
+// Custom Middleware
+const middleware2 = (req, res, next) => {
+    console.log('Middleware 2')
+    console.log(`${req.method} ${req.path}`)
+    console.log(req.info)
+    next()
+}
+
+// Global Middlewares
+app.use(middleware1)
+app.use(middleware2)
+app.use((req, res, next) => {
+    console.log('Middleware 3')
+    next()
+})
+
+// Custom Authentication Middleware
+const auth = (req, res, next) => {
+    const { username, password } = req.body
+    console.log('Auth Middleware')
+    if(username === 'admin' && password === 'admin') {
+        next()
+    } else {
+        res.send('Unauthorized')
     }
-    // if(typeof user !== 'object') res.send(json)
+};
 
-    // res.status(200).send('User has been Added')
-    res.redirect('/users')
-    
-    // res.end('Invalid Operation')
+// GET / - Middleware 1 - Auth - Middleware 2
+app.get('/', middleware1, auth, middleware2, (req, res) => {
+    res.send(`Welcome ${req.body.username}`)
 })
 
-// const users = [{
-//     id: 1,
-//     name: 'John'
-// }, {
-//     id: 2,
-//     name: 'Jane'
-// }]
-
-// app.post('/users', (req, res) => {
-//     const user = req.body
-//     // console.log(req.body)
-//     users.push({
-//         id: users.length + 1,
-//         name: req.body.name
-//     })
-//     console.log(users)
-//     res.status(201).send(user)
-// })
-
-// app.get('/users', (req, res) => {
-//     res.send(users)
-// })
-
-app.get('/users/:id', (req, res) => {
-    const {id:myid} = req.params
-    res.send('Responding from /users' + myid)
+app.post('/', (req, res) => {
+    res.send(`Hello World from ${req.method}`)
 })
 
+// Crash the server
+app.get('/comments', (req, res) => {
+    // Code will crash here and sends to Error middleware
+    comments.push(req.body)
+    res.send('Comments')
+})
+
+// Users Router
+const userRouter = require('./userRouter');
+app.use('/users', userRouter);
+
+// When no route is matched
+app.use((req, res) => {
+    res.status(404).send('404 Not Found')
+})
+
+// Error Middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Something broke!')
+})
+
+// Listen on port 3000
 app.listen(3000, () => {
-    console.log('Listening on Port: 3000')
+    console.log(`Listening on port: 3000`)
 })
